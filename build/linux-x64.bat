079012e2feacd23de01865269f96cfc5ea32ae58
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2025-09-08 16:15:36 +0300

    Linux export preset

--- build/linux-x64.bat
+++ build/linux-x64.bat
@@ -1,17 +1,17 @@
 @echo off
 cls
-echo ======================
-echo    WINDOWS EXPORTER
-echo ======================
+echo =====================
+echo    LINUX EXPORTER
+echo =====================
 
 set GODOT_PATH="C:\Program Files\Godot_v4.4.1-stable_mono_win64\Godot_v4.4.1-stable_mono_win64.exe"
 set PROJECT_PATH=%~dp0..\src\SteamPanno
-set EXPORT_PATH=%~dp0..\dist\windows-x64
+set EXPORT_PATH=%~dp0..\dist\linux-x64
 
 echo Exporting...
 
 mkdir "%EXPORT_PATH%" 2> NUL
-%GODOT_PATH% --verbose --headless --path "%PROJECT_PATH%" --export-release "Windows Desktop" "%EXPORT_PATH%\SteamPanno.exe"
+%GODOT_PATH% --verbose --headless --path "%PROJECT_PATH%" --export-release "Linux" "%EXPORT_PATH%\SteamPanno.exe"
 if %errorlevel% equ 0 (
     echo Export successful!
 ) else (
